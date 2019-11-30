8353a1ef1eb25c07fd41cecaf756c7d6f59e8998
Prajnan Jyoti Bhuyan <prajnanbhuyan@gmail.com>
2019-11-30 10:40:28 +0530

    added missing domain models

---
+++ Set5/src/Engine/Models/BallotBox.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    class BallotBox
+    {
+        private List<Message> messages;
+
+        public int Count { get { return messages.Count; } }
+
+        public BallotBox()
+        {
+            messages = new List<Message>();
+        }
+
+        /// <summary>
+        /// Drop a message into the ballot box
+        /// </summary>
+        /// <param name="message">The message to be dropped into the ballot box</param>
+        public void PutInMessage(Message message)
+        {
+            messages.Add(message);
+        }
+
+        /// <summary>
+        /// Pick a message from the ballot box.
+        /// </summary>
+        /// <returns>Returns a random message from the ballot box.</returns>
+        public Message PickMessage()
+        {
+            // Randomizer to choose which messages gets picked
+            var rnd = new Random();
+
+            // Randomly choose message index
+            var messageIndex = rnd.Next(Count);
+
+            // Randomly choosen message
+            var message = messages[messageIndex];
+
+            // Remove message from list so that it is not sent out a second time
+            messages.RemoveAt(messageIndex);
+
+            // Return message
+            return message;
+        }
+    }
+}
---
+++ Set5/src/Engine/Models/HighPriest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    class HighPriest
+    {
+        int messagesToChoose;
+
+        public HighPriest(int messagesToChoose)
+        {
+            this.messagesToChoose = messagesToChoose;
+        }
+
+        public List<Message> PickRandomMessages(BallotBox ballotBox)
+        {
+            // In some cases the number of alliance requests might be less 
+            // than the number of messages the high priest intended to select
+            messagesToChoose = Math.Min(ballotBox.Count, messagesToChoose);
+
+            var choosenMessages = new List<Message>();
+
+            // The High Priest of Southeros chooses the messages to send out
+            for (int i = 0; i < messagesToChoose; i++)            
+                choosenMessages.Add(ballotBox.PickMessage());
+
+            return choosenMessages;
+        }
+
+        public void HandOverMessages()
+        { 
+        
+        }
+    }
+}
